af87781e3fc9014a39ecb86127b18fba671776f4
Полина <ahahlinapp@gmail.com>
2025-01-18 18:45:44 +0500

    Добавлены переменные и их вывод

--- greetings.py
+++ greetings.py
@@ -4,6 +4,7 @@ content = "list of products"
 status = "Web"
 created_date = "17.01.2025"
 issue = "17.02.2025"
+
 print("Имя пользователя:", username)
 print("Заголовок заметки",title)
 print("Описание заметки",content)
