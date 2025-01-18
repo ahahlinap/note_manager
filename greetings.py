395cd8660a986c1bea24219098852442034e5635
Полина <ahahlinapp@gmail.com>
2025-01-18 18:20:37 +0500

    Добавлено имя пользователя

--- greetings.py
+++ greetings.py
@@ -1,3 +1,12 @@
 username = "polina"
-
+title = "Products"
+content = "list of products"
+status = "Web"
+created_date = "17.01.2025"
+issue = "17.02.2025"
 print("Имя пользователя:", username)
+print("Заголовок заметки",title)
+print("Описание заметки",content)
+print("Статус заметки", status)
+print("Дата создания заметки",created_date)
+print("Дата истечения заметки",issue)
